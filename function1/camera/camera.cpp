a1cfd8158028d04bc37d31dd86ad2ae10171c125
by-hwa <bu04007@gmail.com>
2026-08-28 16:16:01 +0900

    implement camera module

--- function1/camera/camera.cpp
+++ function1/camera/camera.cpp
@@ -12,10 +12,25 @@ std::string Camera::capture()
     std::cout << "[Camera] Capturing image...\n";
 
     // TODO:
-    // Raspberry Pi Camera 실제 촬영 코드 구현
+    // Raspberry Pi Camera 실제 촬영 코드 구현 TEST 필
 
-    std::string image_path =
-        "/tmp/scene.jpg";
+    const std::string image_path = "/tmp/scene.jpg";
+
+    std::string command =
+        "rpicam-still "
+        "--output " + image_path +
+        " --width 640 "
+        " --height 480 "
+        " --timeout 100 "
+        "--nopreview";
+
+    int result = std::system(command.c_str());
+
+    if (result != 0)
+    {
+        std::cerr << "[Camera] Failed to capture image\n";
+        return "";
+    }
 
     std::cout << "[Camera] Image: "
               << image_path << '\n';
